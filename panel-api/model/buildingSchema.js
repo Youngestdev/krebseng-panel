ecaf00da6612180a6c5bd3b30211ec76f284f514
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-22 23:29:40 +0100

    Building schema for mongoose database

---
+++ panel-api/model/buildingSchema.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const Schema = mongoose.Schema;
+
+const BuildingSchema = new Schema({
+    title: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true,
+      default: Date.now()
+    },
+    address: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    neighborhood: {
+      type: String,
+      trim: true,
+      required: true
+    }
+  },
+  {collection: 'building'}
+);
+
+BuildingSchema.index({title: 1}, {unique: true});
+
+let BuildingModel = mongoose.model('Building', BuildingSchema);
+
+module.exports = BuildingModel;
