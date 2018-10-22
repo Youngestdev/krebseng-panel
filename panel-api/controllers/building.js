453d388e40426decf60c07f1fa67ab9770811b58
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-22 23:27:39 +0100

    Handle the operations for the building root. Currently under development

---
+++ panel-api/controllers/building.js
@@ -0,0 +1,34 @@
+const BuildingModel = require("../model/buildingSchema");
+
+exports.list = (req, res) => {
+  const user = req.body.name;
+
+  BuildingModel.find(user).then(
+    building => {
+      res.json(building);
+    }
+  )
+    .catch(err => {
+      console.log(err);
+      res.status(422).send(err.errors);
+    });
+
+};
+
+exports.get = (req, res) => {
+  BuildingModel.findById(req.params.id).exec((err, building) => {
+    if (err) res.status(404).send(err);
+    res.json(building)
+  })
+};
+
+exports.create = (req, res) => {
+  const building = new BuildingModel(req.body);
+  building.save()
+    .then(building => {
+      res.json('Server added successfully');
+    })
+    .catch(err => {
+      res.status(400).send("Unable to create new item");
+    });
+};
