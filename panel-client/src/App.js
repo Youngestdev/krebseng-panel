d7d9840f127ec122a5714ca853050ec8e022f1da
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-24 01:16:13 +0100

    Updated routes and cleaned code

--- panel-client/src/App.js
+++ panel-client/src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar/NavBar';
-import {Route} from 'react-router-dom'
+import {Route, withRouter} from 'react-router-dom'
 import NewBuilding from "./Building/NewBuilding";
 import Buildings from "./Building/Buildings";
 import Building from "./Building/Building";
+import auth0Client from "./Auth/Auth";
 import Callback from "./Callback/Callback";
 import SecuredRoute from "./SecuredRoute/SecuredRoute";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkingSession: true,
+    }
+   }
+  async componentDidMount() {
+    if (this.props.location.pathname === '/callback') {
+	this.setState({checkingSession: false});
+	return;
+    }
+    try {
+      await auth0Client.silentAuth();
+      this.forceUpdate();
+    } catch (err) {
+      if (err.error === 'login_required') return;
+      console.log(err.error);
+    }
+    this.setState({checkingSession: false});
+  }
+
   render() {
     return (
       <div>
         <NavBar/>
-        <Route exact path='/buildings' component={Buildings}/>
+        <SecuredRoute path='/panel/new-building' component={NewBuilding} checkingSession={this.state.checkingSession}/>
+        <SecuredRoute exact path='/panel/buildings' component={Buildings} checkingSession={this.state.checkingSession}/>
         <Route exact path='/panel/building/:id' component={Building}/>
         <Route exact path='/callback' component={Callback}/>
-        <SecuredRoute path='/new-building' component={NewBuilding}/>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
