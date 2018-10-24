f732bd99093c6005f08ebc938800259647c9eb58
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-24 01:16:52 +0100

    Session keeping added and code fixes

--- panel-client/src/Auth/Auth.js
+++ panel-client/src/Auth/Auth.js
@@ -3,7 +3,6 @@ import auth0 from 'auth0-js';
 class Auth {
   constructor() {
     this.auth0 = new auth0.WebAuth({
-      // the following three lines MUST be updated
       domain: 'budul.auth0.com',
       audience: 'https://budul.auth0.com/userinfo',
       clientID: 'H5WocKc729qvrAw8L3nKSUN7srQQdIxq',
@@ -17,19 +16,20 @@ class Auth {
     this.isAuthenticated = this.isAuthenticated.bind(this);
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
-    this.setSession = this.setSession.bind(this);
-    // this.getIdToken = this.getIdToken.bind(this);
   }
 
   getProfile() {
     return this.profile;
   }
 
+  getIdToken() {
+    return this.idToken;
+  }
+
   handleAuthentication() {
     return new Promise((resolve, reject) => {
       this.auth0.parseHash((err, authResult) => {
         if (err) return reject(err);
-        console.log(authResult);
         if (!authResult || !authResult.idToken) {
           return reject(err);
         }
@@ -43,29 +43,35 @@ class Auth {
     return new Date().getTime() < this.expiresAt;
   }
 
-  login() {
-    this.auth0.authorize();
-  }
-
-  logout() {
-    // clear id token and expiration
-    this.idToken = null;
-    this.expiresAt = null;
-  }
-
-  setSession(authResult) {
+  setSession(authResult, step) {
     this.idToken = authResult.idToken;
     this.profile = authResult.idTokenPayload;
     // set the time that the id token will expire at
     this.expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
   }
 
-  getIdToken() {
-    return this.idToken;
+  login() {
+    this.auth0.authorize();
   }
 
+  logout() {
+    this.auth0.logout({
+      returnTo: 'http://localhost:3000',
+      clientID: 'H5WocKc729qvrAw8L3nKSUN7srQQdIxq',
+    });
+  }
+
+  silentAuth() {
+    return new Promise((resolve, reject) => {
+      this.auth0.checkSession({}, (err, authResult) => {
+        if (err) return reject(err);
+        this.setSession(authResult);
+        resolve();
+      });
+    });
+  }
 }
 
 const auth0Client = new Auth();
 
-export default  auth0Client;
+export default auth0Client;
