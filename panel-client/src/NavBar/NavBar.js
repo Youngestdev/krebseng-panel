b0b8d59db4957ea1c22fe7ded5e5e8dd8c859def
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-27 02:28:18 +0100

    Spacing

--- panel-client/src/NavBar/NavBar.js
+++ panel-client/src/NavBar/NavBar.js
@@ -9,13 +9,14 @@ function NavBar(props) {
   };
 
   return (
-    <nav className="navbar navbar-dark bg-primary fixed-top">
+      <nav className="navbar navbar-dark bg-primary fixed-top">
       <Link className="navbar-brand" to="/panel/buildings">
         KrebsEng
       </Link>
       {
         !auth0Client.isAuthenticated() && <button className="btn btn-dark" onClick={auth0Client.login}>Sign In</button>
       }
+
       {
         auth0Client.isAuthenticated() && <div>
             <button className="btn btn-dark" onClick={() => {signOut()}}>Sign Out</button>
