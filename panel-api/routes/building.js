9251932cd95a47af0eac51f0a333c9f4a33711e0
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-27 02:23:59 +0100

    Protected route

--- panel-api/routes/building.js
+++ panel-api/routes/building.js
@@ -4,7 +4,7 @@ const panel = express.Router();
 const checkJwt = require('../middleware/auth');
 
 panel.get('/buildings', checkJwt, Building.list);
-panel.get('/building/:id', Building.get);
+panel.get('/building/:id', checkJwt, Building.get);
 panel.post('/building', checkJwt, Building.create);
 panel.put('/building/:id', checkJwt, Building.update);
 panel.delete('/building/:id', checkJwt, Building.delete);
