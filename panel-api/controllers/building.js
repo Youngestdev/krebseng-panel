68c1dc74f05c35aa37c6523e51e57d4401b2e176
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-27 02:22:56 +0100

    Created the update and delete route working perfectly & fixed minor bugs

--- panel-api/controllers/building.js
+++ panel-api/controllers/building.js
@@ -25,9 +25,35 @@ exports.create = (req, res) => {
   const building = new BuildingModel(req.body);
   building.save()
     .then(building => {
-      res.json('Server added successfully');
+      res.json('Item added successfully');
     })
     .catch(err => {
       res.status(400).send("Unable to create new item");
+    });
+};
+
+exports.update = (req, res) => {
+  BuildingModel.updateOne({_id: req.params.id}, {
+    $set:
+      {
+        title: req.body.title,
+        address: req.body.address,
+        neighborhood: req.body.neighborhood,
+        date: req.body.date
+      }
+  }).then(building => {
+    res.json("Item updated successfully");
+  })
+    .catch(err => {
+      res.status(400).send("Unable to update item.")
+    });
+};
+
+exports.delete = (req, res) => {
+  BuildingModel.findByIdAndRemove({_id: req.params.id}).then(building => {
+    res.json("Item deleted successfully");
+  })
+    .catch(err => {
+      res.status(400).send("Unable to delete item");
     });
 };
