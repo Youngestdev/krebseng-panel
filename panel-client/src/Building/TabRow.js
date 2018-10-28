db466642d6d2ef3567a39c8eaff8e7f9ed1f18ee
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-28 01:05:16 +0100

    Fixed a couple of things, formating date next.

--- panel-client/src/Building/TabRow.js
+++ panel-client/src/Building/TabRow.js
@@ -2,24 +2,25 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import auth0Client from '../Auth/Auth';
-import {ParseDate} from '../Helpers/ParseDate';
+// import {ParseDate} from '../Helpers/ParseDate';
 
 class TableRow extends Component {
 
   render() {
     return (
       <tr>
-        <td><Link to={`/panel/building/${this.props.obj._id}`}>{this.props.obj.title}</Link></td>
-        <td>{ParseDate(this.props.obj.date)}</td>
+        {/* TODO: Format Date ! */}
+        <td><h5>{this.props.obj.title}</h5></td>
+        <td>{this.props.obj.date}</td>
         <td className="right">
-          <Link className="btn btn-secondary my-2 my-sm-0" to={`/panel/update-building/${this.props.obj._id}`}>Edit</Link>
+          <Link className="btn btn-primary btn-sm" to={`/panel/update-building/${this.props.obj._id}`}>Edit</Link>
           {/*Inline function to execute post request*/}
-          <button className="btn btn-danger my-2 my-sm-0" onClick={() => {
+          <button className="btn btn-danger btn-sm" onClick={() => {
             axios.delete(`http://localhost:8000/panel/building/${this.props.obj._id}`, {
               headers: {'Authorization': `Bearer ${auth0Client.getIdToken()}`}
             })
               .then(res => console.log(res.data));
-            this.props.history.replace('/panel/buildings');
+              window.location.reload(true);
           }}>Delete</button>
         </td>
       </tr>
