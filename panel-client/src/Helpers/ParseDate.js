1265a175c23b84ecd2a2bd61699e9ca55f92d733
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2018-10-28 01:05:52 +0100

    Temporary - It's subject to change

--- panel-client/src/Helpers/ParseDate.js
+++ panel-client/src/Helpers/ParseDate.js
@@ -1,5 +1,5 @@
 // Parse Date from database to a simpler format.
 export const ParseDate = date => {
   date = new Date(date);
-  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDay()}`
+  return `${date.getDay() + 1}/${date.getMonth() + 1}/${date.getFullYear()}`
 };
